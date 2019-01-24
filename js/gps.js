add3de078747743dccd8abfa04be0124fc174a17
jurcabogdan <47008241+jurcabogdan@users.noreply.github.com>
2019-01-24 20:44:07 +0200

    Add files via upload

---
+++ js/gps.js
@@ -0,0 +1,16 @@
+document.getElementById("id_logic_version").innerHTML = "Logic version=2019.01.11.0";
+document.getElementById("id_button").addEventListener("click", on_gps_position);
+
+function on_gps_ok(e){
+    document.getElementById("id_lat").innerHTML = e.coords.latitude;
+    document.getElementById("id_long").innerHTML = e.coords.longitude;
+    document.getElementById("id_acc").innerHTML = e.coords.accuracy;
+}
+
+function on_gps_error(e){
+    alert("error")
+}
+
+function on_gps_position(){
+    navigator.geolocation.getCurrentPosition(on_gps_ok, on_gps_error);
+}
